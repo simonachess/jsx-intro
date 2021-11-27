b4470ddc079953f9e4bf8d83ac896394bb811ede
simonachess <simonachess@gmail.com>
2021-11-27 16:12:37 +0200

    added like link

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -5,8 +5,20 @@ class Post extends React.Component {
 
     constructor(props) {
         super(props);
+        this.state = {
+            counter: 0,
+        }
+        this.handleLike = this.handleLike.bind(this)
     }
 
+    handleLike() {
+        this.setState((prevState => {
+            return {
+                counter: prevState.counter + 1
+            }
+        }))
+
+    }
 
     render() {
         return (
@@ -14,6 +26,8 @@ class Post extends React.Component {
                 <div className="feature bg-primary bg-gradient text-white rounded-3 mb-4 mt-n4"><i className="bi bi-collection"></i></div>
                 <h2 className="fs-4 fw-bold">{this.props.title}</h2>
                 <p className="mb-0">{this.props.about}</p>
+                <p>Like: {this.state.counter}</p>
+                <a href="/#" onClick={this.handleLike}>Like</a>
             </Card>
 
         )
